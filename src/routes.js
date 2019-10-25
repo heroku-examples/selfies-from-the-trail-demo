93c534764f38aca422fbf310bc0017e63b4a8d3d
Luke Karrys <lukekarrys@gmail.com>
2019-10-25 13:40:13 -0700

    add todo

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const controllers = require('./controllers')
 
 module.exports = [
+  // TODO: add route for saving image to s3 and producing kafka event
+  // and use public url of image to share to twitter
   {
     method: 'POST',
     path: '/api/submit',
