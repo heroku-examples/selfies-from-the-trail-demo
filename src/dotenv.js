a9298943433f197933305d0057826cc3be34ffb8
Luke Karrys <lukekarrys@gmail.com>
2019-11-05 15:41:18 -0700

    Make request to server app at initial start up

--- src/dotenv.js
+++ src/dotenv.js
@@ -1,3 +1,37 @@
+const { execSync } = require('child_process')
+
+const curl = (...cmds) =>
+  JSON.parse(
+    execSync(`curl -n -o - ${cmds.join(' \\\n')}`, { stdio: 'pipe' }).toString()
+  )
+
+const herokuCurl = ({ endpoint = '', appName, token }) => {
+  let res
+
+  try {
+    res = curl(
+      `https://api.heroku.com/apps/${appName}/${endpoint}`,
+      `-H "Accept: application/vnd.heroku+json; version=3"`,
+      `-H "authorization: Bearer ${token}"`
+    )
+  } catch (e) {
+    throw new Error(
+      `Failed to get ${appName} config vars via curl\n${e.stderr.toString()}`
+    )
+  }
+
+  if (res.id && res.message) {
+    throw new Error(`Heroku API Error: ${res.id} - ${res.message}`)
+  }
+
+  return res
+}
+
+// Use console.log for this since this file has to be required first so we
+// dont have any loggers set up yet
+// eslint-disable-next-line no-console
+const log = console.log
+
 if (process.env.NODE_ENV === 'production') {
   // To make this app easier to setup, in production the only env vars it needs to be bootstrapped
   // with are the name of a heroku app and a token that can be used to fetch all
@@ -10,34 +44,26 @@ if (process.env.NODE_ENV === 'production') {
     )
   }
 
-  let config
-  try {
-    config = JSON.parse(
-      require('child_process')
-        .execSync(
-          `curl -n -o - https://api.heroku.com/apps/${SERVER_APP_NAME}/config-vars \
-            -H "Accept: application/vnd.heroku+json; version=3" \
-            -H "authorization: Bearer ${HEROKU_TOKEN}"`,
-          { stdio: 'pipe' }
-        )
-        .toString()
-    )
-  } catch (e) {
-    throw new Error(
-      `Failed to get ${SERVER_APP_NAME} config vars via curl\n${e.stderr.toString()}`
-    )
-  }
+  const { web_url } =
+    { web_url: 'http://localhost:8080/' } ||
+    herokuCurl({
+      appName: SERVER_APP_NAME,
+      token: HEROKU_TOKEN
+    })
 
-  if (config.id) {
-    throw new Error(`Heroku API Error: ${config.id} - ${config.message}`)
-  }
+  const serverAppStatusUrl = `${web_url}api/send-attendee-status`
+  const status = curl(`-X POST ${serverAppStatusUrl}`)
+  log(`POSTing to ${serverAppStatusUrl} - ${JSON.stringify(status)}`)
+
+  const config = herokuCurl({
+    appName: SERVER_APP_NAME,
+    token: HEROKU_TOKEN,
+    endpoint: 'config-vars'
+  })
 
   Object.keys(config).forEach(function(key) {
     if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
-      // Use console.log for this since this file has to be required first so we
-      // dont have any loggers set up yet
-      // eslint-disable-next-line no-console
-      console.log(`Setting env var for ${key} from ${SERVER_APP_NAME}`)
+      log(`Setting env var for ${key} from ${SERVER_APP_NAME}`)
       process.env[key] = config[key]
     }
   })
